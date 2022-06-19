ec0d94811a4db0eba44a872b292bbcee4d31d8e5
HoanNguyen <nhoxxu197@gmail.com>
2022-06-20 00:17:51 +0700

    Update header (responsive) and fix navbar(UI)

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,20 +6,20 @@ import ai from '../../assets/ai.png';
 const Header = () => {
   return (
     <div className="gpt3__header">
-      <div className="gpt3__header_content">
+      <div className="gpt3__header-content">
         <h2>Let’s Build Something amazing with GPT-3 OpenAI</h2>
         <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
         <form>
-          <input type="text" name="search" placeholder="Your Email Address" />
+          <input type="email" name="search" placeholder="Your Email Address" />
           <button><span>Get Start</span></button>
         </form>
-        <div className="gpt3__header_content_signup">
+        <div className="gpt3__header-content__signup">
           <img src={people} />
           <span>1,600 people requested access a visit in last 24 hours</span>
         </div>
       </div>
 
-      <div className="gpt3__header_illustration">
+      <div className="gpt3__header-illustration">
         <img src={ai} alt="ai" />
       </div>
     </div>
