a63e4917f781d53cc91d8f1615bd288b67ed7baf
HoanNguyen <nhoxxu197@gmail.com>
2022-06-20 12:35:39 +0700

    Create WhatGPT3 section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -3,7 +3,56 @@ import './whatGPT3.css'
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div class='gpt3__whatGPT3 gradient__bg'>
+      <div className="gpt3__whatGPT3__top">
+        <div className="gpt3__whatGPT3-content">
+          <div className="gpt3__whatGPT3-content__header">
+            <div></div>
+            <h2>What is GPT-3</h2>
+          </div>
+          <div className="gpt3__whatGPT3-content__text">
+            We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.
+          </div>
+        </div>
+      </div>
+
+      <div className="gpt3__whatGPT3__mid">
+        <h1>The possibilities are beyond your imagination</h1>
+        <h2>Explore The Library</h2>
+      </div>
+
+      <div className="gpt3__whatGPT3__bottom">
+        <div className="gpt3__whatGPT3-content">
+          <div className="gpt3__whatGPT3-content__header">
+            <div></div>
+            <h2>Chatbots</h2>
+          </div>
+          <div className="gpt3__whatGPT3-content__text">
+            We so opinion friends me message as delight. Whole front do of plate heard oh ought.
+          </div>
+        </div>
+
+        <div className="gpt3__whatGPT3-content">
+          <div className="gpt3__whatGPT3-content__header">
+            <div></div>
+            <h2>Knowledgebase</h2>
+          </div>
+          <div className="gpt3__whatGPT3-content__text">
+            At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b
+          </div>
+        </div>
+
+        <div className="gpt3__whatGPT3-content">
+          <div className="gpt3__whatGPT3-content__header">
+            <div></div>
+            <h2>Education</h2>
+          </div>
+          <div className="gpt3__whatGPT3-content__text">
+            At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
