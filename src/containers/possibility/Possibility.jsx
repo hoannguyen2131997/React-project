e470ec9f3d3bb782aaf221518463217d6214e56f
HoanNguyen <nhoxxu197@gmail.com>
2022-06-21 16:20:00 +0700

    Create possibility container

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './possibility.css'
+import possibility from '../../assets/possibility.png';
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div class="gpt3__possibility">
+      <div className="gpt3__possibility-img">
+        <img src={possibility} alt="" />
+      </div>
+
+      <div className="gpt3__possibility-content">
+        <h2>Request Early Access to Get Started</h2>
+        <h1>The possibilities are beyond your imagination</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <h3>Request Early Access to Get Started</h3>
+      </div>
+    </div>
   )
 }
 
