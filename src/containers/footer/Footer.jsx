6f22d01f42d7ef31f47e3fa0fd24cf7d8df49951
HoanNguyen <nhoxxu197@gmail.com>
2022-06-23 11:43:02 +0700

    Create footer container

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import './footer.css'
-
+import logo from '../../assets/logo.svg';
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div class="gpt3__footer">
+      <h1>Do you want to step in to the future before others</h1>
+      <button><span>Request Early Access</span></button>
+      <div className="gpt3__footer-content">
+
+        <div className="gpt3__footer-content__band">
+          <img src={logo} alt="logo" />
+          <span>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</span>
+        </div>
+
+        <div className="gpt3__footer-content__list">
+          <div>
+            <h3>Links</h3>
+            <p>Overons</p>
+            <p>Social Media</p>
+            <p>Counters</p>
+            <p>Contact</p>
+          </div>
+
+          <div>
+            <h3>Company</h3>
+            <p>Terms & Conditions</p>
+            <p>Privacy Policy</p>
+            <p>Contact</p>
+          </div>
+
+          <div>
+            <h3>Get in touch</h3>
+            <p>Crechterwoord K12 182 DK Alknjkcb</p>
+            <p>085-132567</p>
+            <p>info@payme.net</p>
+          </div>
+        </div>
+      </div>
+      
+      <div className="gpt3__footer-info">
+        <span>© 2021 GPT-3. All rights reserved.</span>
+      </div>
+    </div>
   )
 }
 
